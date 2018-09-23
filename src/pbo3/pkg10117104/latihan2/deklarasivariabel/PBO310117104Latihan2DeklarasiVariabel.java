dc9e46546710acfd3d442507f68b25176d69a067
Aditya Suheryana <adityasuheryanaa@gmail.com>
2018-09-23 17:31:41 +0700

    first commit

---
+++ src/pbo3/pkg10117104/latihan2/deklarasivariabel/PBO310117104Latihan2DeklarasiVariabel.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author
+ * Nama      : Aditya Suheryana
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini Mencontohkan cara Mendeklarasikan Variabel
+ */
+public class PBO310117104Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    //Deklarasi Variabel
+    int nilaiInt;
+    final double PHI = 3.14; //konstanta uppercase
+    boolean nilaiLogika;
+    char nilaiKarakter;
+    
+    //Memberi nilai ke variabel
+    nilaiInt = 78;
+    nilaiLogika = false;
+    nilaiKarakter = 'D';
+    
+    //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+        
+        
+    
+    }
+    
+}
